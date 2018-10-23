3a33c5733e0a839186ec2bd5e6b5a69fe4f58177
Ludwig <loic.frueh@gmail.com>
2018-10-23 14:32:09 +0200

    Read Option Part1 Done

--- site/html/includes/nav.php
+++ site/html/includes/nav.php
@@ -1,7 +1,16 @@
 <!-- Navigation -->
 <nav class="navbar navbar-default navbar-static-top m-b-0">
     <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="fa fa-bars"></i></a>
-        <div class="top-left-part"><a class="logo" href="mailbox.php"><b><img src="plugins/images/pixeladmin-logo.png" alt="home" /></b><span class="hidden-xs"><img src="plugins/images/pixeladmin-text.png" alt="home" /></span></a></div>
+        <div class="top-left-part">
+            <a class="logo" href="mailbox.php">
+                <b><img src="plugins/images/pixeladmin-logo.png" alt="home" /></b>
+                <span class="hidden-xs">
+                    <span class="title">
+                        <span class="bold">Post</span>mail
+                    </span>
+                </span>
+            </a>
+        </div>
         <ul class="nav navbar-top-links navbar-right pull-right">
             <li>
                 <a class="profile-pic" href="mailbox.php"> <img src=" plugins/images/users/varun.jpg" alt="user-img" width="36" class="img-circle"><b class="hidden-xs"><?php echo $_SESSION['user']; ?></b> </a>
---
+++ site/html/read_mess.php
@@ -0,0 +1,131 @@
+<?php session_start(); ?>
+<?php include 'includes/connection_check.php'; ?>
+<?php include 'includes/connect.php'; ?>
+<?php
+            if (isset($_GET['id']) ) 
+            {
+                $req = $db->prepare('
+                    SELECT *
+                    FROM Message
+                    WHERE id = :message_id AND idUser = :id_user
+                ');
+
+                $req->execute( array(
+                    'message_id' => $_GET['id'],
+                    'id_user' => $_SESSION['id']
+                ));
+
+                $req2 = $db->prepare('
+                    SELECT username
+                    FROM user
+                    WHERE id = :user_id
+                ');
+
+                $req2->execute( array(
+                    'user_id' => 1,  //TODO: A CHANGER PAR LE BON ID
+                ));
+
+                $result_mess = $req->fetch();
+                $result_user = $req2->fetch();
+
+                if (empty($result_mess) ) 
+                {
+                    header("Location: mailbox.php");
+                    exit();
+                }
+
+                $id_mess = $result_mess['id']; 
+                $from = $result_user['username'];
+                $subject = $result_mess['subject'];
+                $message = $result_mess['message'];
+                $date = $result_mess['date'];
+            }
+            else 
+            {
+                header("Location: mailbox.php");
+                exit();   
+            }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Postmail - Read message</title>
+    <?php include 'includes/link.php'; ?>
+</head>
+
+<body>
+    <div id="wrapper">
+        <!-- Header and Left Menu -->
+        <?php include 'includes/nav.php'; ?>
+        <!-- Left navbar-header end -->
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <!-- /row -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="white-box">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>From</th>
+                                            <th class="date"><?php echo $date; ?></th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <tr>
+                                            <td><?php echo $from; ?></td>
+                                        </tr>                
+                                    </tbody>
+
+                                    <thead>
+                                        <tr>
+                                            <th>Subject</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><?php echo $subject; ?></td>
+                                        </tr>                
+                                    </tbody>
+
+                                    <thead>
+                                        <tr>
+                                            <th>Message</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><?php echo $message; ?></td>
+                                        </tr>                
+                                    </tbody>
+
+                                </table>
+                            </div>
+                            <a href="#" class="btn btn-success link">Respond</a>
+                            <a href="#" class="btn-danger btn link">Delete</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+            <footer class="footer text-center"> 2017 &copy; Pixel Admin brought to you by wrappixel.com </footer>
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <?php include 'includes/scripts.php'; ?>
+</body>
+
+</html>
