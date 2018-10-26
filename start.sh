36ec7f35241018ed28343cee68afab0a008bd4b1
Adam <adam.zouari@heig-vd.ch>
2018-10-26 15:22:51 +0200

    update script

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# kill the docker if already running
+sudo docker rm -f sti_project 2> /dev/null 1> /dev/null
+
+sudo docker run -ti -v "$PWD/site":/usr/share/nginx/ -d -p 8080:80 --name sti_project --hostname sti arubinst/sti:project2018
+
+sudo docker exec -u root sti_project service nginx start
+sudo docker exec -u root sti_project service php5-fpm start
+sudo docker exec -u root sti_project chmod a+wx /usr/share/nginx/databases /usr/share/nginx/databases/db.sqlite
+
+echo "Web interface PostMail launching ..."
+xdg-open localhost:8080 &
